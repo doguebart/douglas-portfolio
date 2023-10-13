917b034e955d676694f35e3207d32c99a775dce2
doguebart <douglasweelber.2004@outlook.com>
2023-10-13 17:35:15 -0300

    feat: creating experience section

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,10 +1,45 @@
 import React from "react";
+import { FiLinkedin, FiGithub, FiFile } from "react-icons/fi";
+import { Link } from "react-router-dom";
 import Header from "./components/header";
+import AboutMe from "./components/sections/aboutMe";
+import Projects from "./components/sections/projects";
+import Experience from "./components/sections/experience";
 
 const App = () => {
   return (
-    <div className="w-full h-full bg-cyan-950">
+    <div className="w-full h-full bg-white flex flex-col">
       <Header />
+      <div className="w-full max-h-full flex flex-col">
+        <div
+          className="w-full h-screen flex flex-col items-center justify-center bg-cyan-900"
+          id="start"
+        >
+          <h2 className="text-2xl md:text-4xl text-white text-center">
+            Olá, me chamo <br />{" "}
+            <strong className="text-4xl md:text-6xl text-cyan-500">
+              Douglas Welber
+            </strong>
+          </h2>
+          <span className="mt-4 text-white/50 font-normal">
+            Programador Frontend
+          </span>
+          <div className="max-w-full mt-4 max-h-full flex items-center space-x-4">
+            <Link className="p-2 bg-white shadow-md m-auto rounded-full hover:opacity-90 duration-200">
+              <FiLinkedin className="text-xl text-cyan-950" />
+            </Link>
+            <Link className="p-2 bg-white shadow-md m-auto rounded-full hover:opacity-90 duration-200">
+              <FiGithub className="text-xl text-cyan-950" />
+            </Link>
+            <Link className="p-2 bg-white shadow-md m-auto rounded-full hover:opacity-90 duration-200">
+              <FiFile className="text-xl text-cyan-950" />
+            </Link>
+          </div>
+        </div>
+        <AboutMe />
+        <Projects />
+        <Experience />
+      </div>
     </div>
   );
 };
---
+++ portfolio/src/components/sections/experience/index.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+
+const Experience = () => {
+  return (
+    <div
+      className="lg:w-2/3 px-4 py-4 lg:px-10 self-center max-h-full"
+      id="experience"
+    >
+      <div className="w-full max-h-full mt-20">
+        <h1 className="text-black text-3xl font-bold ">
+          Experiência Profissional
+        </h1>
+        <p className="text-black text-md lg:text-md py-6">
+          Sou um programador frontend com um ano de experiência, trabalhando
+          como freelancer e colaborando em projetos que abrangeram uma variedade
+          de tecnologias. Durante nove meses, concentrei-me em Next.js,
+          TypeScript e Styled-Components, enquanto na última posição, utilizei
+          HTML, CSS, JavaScript e Bootstrap.
+        </p>
+        <div className="w-full max-h-full mt-10 grid grid-cols-1 lg:grid-cols-2 gap-2">
+          <div className="w-full max-h-full flex flex-col p-2 bg-wihte shadow-lg border">
+            <div className="w-full max-h-full flex items-center justify-between">
+              <span className="text-md lg:text-lg text-black font-bold">
+                Programador Frontend
+              </span>
+              <span className="text-sm text-black/50">
+                06/2023 - Presente
+              </span>
+            </div>
+            <div className="w-full max-h-full flex flex-col">
+              <span className="text-black text-md">
+                SIM Instituto Social | Freelancer
+              </span>
+            </div>
+            <p className="mt-4 text-sm text-black/50">
+              Sou o responsável pela criação e desenvolvimento de novas páginas
+              web e componentes interativos, utilizando tecnologias, como HTML,
+              CSS e Javascript. Minha contribuição é fundamental para aprimorar
+              a presença online do instituto, garantindo uma experiência de
+              usuário atraente e eficaz. Além disso, utilizo o Bootstrap para a
+              estilização e tornar as páginas altamente responsivas em diversos
+              dispositivos, melhorando assim a acessibilidade e a usabilidade
+              para um público mais amplo.
+            </p>
+          </div>
+          <div className="w-full max-h-full flex flex-col p-2 bg-white shadow-lg border">
+            <div className="w-full max-h-full flex items-center justify-between">
+              <span className="text-md lg:text-lg text-black font-bold">
+                Programador Frontend
+              </span>
+              <span className="text-sm text-black/50">11/2022 - 06/2023</span>
+            </div>
+            <div className="w-full max-h-full flex flex-col">
+              <span className="text-black text-md">
+                Tunetraders | Freelancer
+              </span>
+            </div>
+            <p className="mt-4 text-sm text-black/50">
+              Utilizando Next.js, TypeScript e Styled-Components, fui
+              responsável pelo desenvolvimento de novas páginas, funcionalidades
+              e aprimoramentos de estilo, com foco na otimização da
+              responsividade nas áreas de dashboard destinadas aos usuários
+              autenticados
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
