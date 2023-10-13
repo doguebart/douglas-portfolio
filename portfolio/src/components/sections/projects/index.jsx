c1187a9e66c157bffb4fd47e76c9e7616b7c8110
doguebart <douglasweelber.2004@outlook.com>
2023-10-13 17:05:57 -0300

    feat: creating projects section

---
+++ portfolio/src/components/sections/projects/index.jsx
@@ -0,0 +1,142 @@
+import React from "react";
+
+const Projects = () => {
+  return (
+    <div
+      className="lg:w-2/3 px-4 lg:px-10 py-4 self-center max-h-full"
+      id="projects"
+    >
+      <div className="w-full max-h-full mt-20">
+        <h1 className="text-black text-3xl mb-6 font-bold">Meus Projetos</h1>
+        <div className="w-full max-h-full grid grid-cols-1 lg:grid-cols-2 lg:gap-4">
+          <div className="w-full max-h-full flex flex-col bg-cyan-900 shadow-md rounded-sm p-2 mt-6">
+            <div className="w-full max-h-full mb-4">
+              <img
+                src="/sim.png"
+                alt="project picture"
+                className="w-full max-h-full cover rounded-tl-sm rounded-tr-sm"
+              />
+            </div>
+            <span className="text-white font-bold text-md mb-2">
+              SIM Instituto Social
+            </span>
+            <p className="text-white/50 text-sm">
+              Sou o responsável pela criação e desenvolvimento de novas páginas
+              web e componentes interativos, utilizando tecnologias, como HTML,
+              CSS e Javascript. <br /> <br /> Minha contribuição é fundamental
+              para aprimorar a presença online do instituto, garantindo uma
+              experiência de usuário atraente e eficaz. Além disso, utilizo o
+              Bootstrap para a estilização e tornar as páginas altamente
+              responsivas em diversos dispositivos, melhorando assim a
+              acessibilidade e a usabilidade para um público mais amplo.
+            </p>
+            <div className="max-w-full max-h-full flex items-center">
+              <a
+                href="https://simsertaosocial.org/"
+                target="blank"
+                className="text-white text-sm rounded-sm bg-cyan-950 p-2 mt-6 shadow-md hover:bg-cyan-950/80 duration-200"
+              >
+                Ver Projeto
+              </a>
+            </div>
+          </div>
+          <div className="w-full max-h-full flex flex-col bg-cyan-900 shadow-md rounded-sm p-2 mt-6">
+            <div className="w-full max-h-full mb-4">
+              <img
+                src="/sim.png"
+                alt="project picture"
+                className="w-full max-h-full cover rounded-tl-sm rounded-tr-sm"
+              />
+            </div>
+            <span className="text-white font-bold text-md mb-2">
+              Nome Do Projeto
+            </span>
+            <p className="text-white/50 text-sm">
+              Sou o responsável pela criação e desenvolvimento de novas páginas
+              web e componentes interativos, utilizando tecnologias, como HTML,
+              CSS e Javascript. <br /> <br /> Minha contribuição é fundamental
+              para aprimorar a presença online do instituto, garantindo uma
+              experiência de usuário atraente e eficaz. Além disso, utilizo o
+              Bootstrap para a estilização e tornar as páginas altamente
+              responsivas em diversos dispositivos, melhorando assim a
+              acessibilidade e a usabilidade para um público mais amplo.
+            </p>
+            <div className="max-w-full max-h-full flex items-center">
+              <a
+                href="https://simsertaosocial.org/"
+                target="blank"
+                className="text-white text-sm rounded-sm bg-cyan-950 p-2 mt-6 shadow-md hover:bg-cyan-950/80 duration-200"
+              >
+                Ver Projeto
+              </a>
+            </div>
+          </div>
+          <div className="w-full max-h-full flex flex-col bg-cyan-900 shadow-md rounded-sm p-2 mt-6">
+            <div className="w-full max-h-full mb-4">
+              <img
+                src="/sim.png"
+                alt="project picture"
+                className="w-full max-h-full cover rounded-tl-sm rounded-tr-sm"
+              />
+            </div>
+            <span className="text-white font-bold text-md mb-2">
+              Nome Do Projeto
+            </span>
+            <p className="text-white/50 text-sm">
+              Sou o responsável pela criação e desenvolvimento de novas páginas
+              web e componentes interativos, utilizando tecnologias, como HTML,
+              CSS e Javascript. <br /> <br /> Minha contribuição é fundamental
+              para aprimorar a presença online do instituto, garantindo uma
+              experiência de usuário atraente e eficaz. Além disso, utilizo o
+              Bootstrap para a estilização e tornar as páginas altamente
+              responsivas em diversos dispositivos, melhorando assim a
+              acessibilidade e a usabilidade para um público mais amplo.
+            </p>
+            <div className="max-w-full max-h-full flex items-center">
+              <a
+                href="https://simsertaosocial.org/"
+                target="blank"
+                className="text-white text-sm rounded-sm bg-cyan-950 p-2 mt-6 shadow-md hover:bg-cyan-950/80 duration-200"
+              >
+                Ver Projeto
+              </a>
+            </div>
+          </div>
+          <div className="w-full max-h-full flex flex-col bg-cyan-900 shadow-md rounded-sm p-2 mt-6">
+            <div className="w-full max-h-full mb-4">
+              <img
+                src="/sim.png"
+                alt="project picture"
+                className="w-full max-h-full cover rounded-tl-sm rounded-tr-sm"
+              />
+            </div>
+            <span className="text-white font-bold text-md mb-2">
+              Nome Do Projeto
+            </span>
+            <p className="text-white/50 text-sm">
+              Sou o responsável pela criação e desenvolvimento de novas páginas
+              web e componentes interativos, utilizando tecnologias, como HTML,
+              CSS e Javascript. <br /> <br /> Minha contribuição é fundamental
+              para aprimorar a presença online do instituto, garantindo uma
+              experiência de usuário atraente e eficaz. Além disso, utilizo o
+              Bootstrap para a estilização e tornar as páginas altamente
+              responsivas em diversos dispositivos, melhorando assim a
+              acessibilidade e a usabilidade para um público mais amplo.
+            </p>
+            <div className="max-w-full max-h-full flex items-center">
+              <a
+                href="https://simsertaosocial.org/"
+                target="blank"
+                className="text-white text-sm rounded-sm bg-cyan-950 p-2 mt-6 shadow-md hover:bg-cyan-950/80 duration-200"
+              >
+                Ver Projeto
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
