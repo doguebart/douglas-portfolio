55334aba26dfa9adae23bf0cc180b3897c09c49d
doguebart <douglasweelber.2004@outlook.com>
2023-10-17 14:06:50 -0300

    fix: adding other about me text

--- portfolio/src/components/sections/aboutMe/index.jsx
+++ portfolio/src/components/sections/aboutMe/index.jsx
@@ -9,15 +9,22 @@ const AboutMe = () => {
       <div className="w-full max-h-full mt-20">
         <h1 className="text-black text-3xl font-bold">Sobre Mim</h1>
         <p className="text-black text-md lg:text-md py-6">
-          Sou um programador frontend com um ano de experiência, trabalhando
-          como freelancer e colaborando em projetos que abrangeram uma variedade
-          de tecnologias. Durante nove meses, concentrei-me em Next.js,
-          TypeScript e Styled-Components, enquanto na última posição, utilizei
-          HTML, CSS, JavaScript e Bootstrap. <br /> <br /> Além disso, moro em Osasco, São
-          Paulo, e em dezembro de 2021 conquistei uma bolsa de estudos e entrei
-          na FIAP para cursar Análise e Desenvolvimento de Sistemas, o que
-          ampliou ainda mais meu conhecimento e habilidades na área de
-          desenvolvimento de software.
+          E aí! Eu sou Douglas Welber e sou de Osasco, São Paulo, Brasil. Em
+          dezembro de 2021, consegui uma bolsa de estudos e entrei na FIAP para
+          estudar Análise e Desenvolvimento de Sistemas, o que foi uma grande
+          oportunidade para eu mergulhar no mundo da programação. <br /> <br />{" "}
+          Eu sou um cara que curte programar, principalmente na parte da frente
+          (frontend). Já tenho um ano de experiência nisso, trabalhando como
+          freelancer e dando uma mãozinha em projetos bem variados. Durante nove
+          meses, eu estava mexendo principalmente com coisas como ReactJs,
+          Next.js, TypeScript Tailwind CSS e Styled-Components, criando sites e
+          aplicativos com visual bacana e responsivos. Na última posição que
+          tive, trabalhei mais com as bases: HTML, CSS e JavaScript, além de
+          usar o Bootstrap para fazer sites funcionais. <br /> <br /> Adoro
+          desafios e estou sempre afim de aprender coisas novas. A sensação de
+          criar algo incrível a partir de linhas de código me empolga muito, e
+          estou sempre buscando maneiras de crescer e aprimorar minhas
+          habilidades.
         </p>
       </div>
     </div>
